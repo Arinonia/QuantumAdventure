9b6803629fbc21575cf170419c26b2af00c3a7f8
Arinonia <arinonia.yt@gmail.com>
2025-04-02 23:12:22 +0200

    add icon

--- src/main/java/fr/quantumadventure/Game.java
+++ src/main/java/fr/quantumadventure/Game.java
@@ -11,5 +11,26 @@ public class Game extends GameApplication {
         settings.setHeight(Constants.GAME_HEIGHT);
         settings.setTitle(Constants.GAME_NAME);
         settings.setVersion(Constants.GAME_VERSION);
+        settings.setAppIcon("images/icon.png");
+    }
+
+    @Override
+    protected void initGame() {
+        System.out.println("Game initialized");
+    }
+
+    @Override
+    protected void initPhysics() {
+        System.out.println("Physics initialized");
+    }
+
+    @Override
+    protected void initUI() {
+        System.out.println("UI initialized");
+    }
+
+    @Override
+    protected void onUpdate(double tpf) {
+        System.out.println("Game updated");
     }
 }
